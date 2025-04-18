3ab380f451cffa50dbba47e38b28fe9c80571023
AlbinFaltstrom <albin.faltstrom@gmail.com>
2025-04-18 10:51:34 +0200

    Added run for dataset 1

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
+import pandas as pd
+import matrix_factorization as MF
 
-#Read data
-data_frame = pd.read_csv('user_artists_hetrec.dat', delimiter='\t')
+def dataset_1():
+    data_frame = pd.read_csv('user_artists_hetrec.dat', delimiter='\t')
 
-#Put data into matrix
-users = data_frame['userID'].values
-items = data_frame['artistID'].values
-weights = data_frame['weight'].values
+    #Put data into matrix
+    users = data_frame['userID'].values
+    items = data_frame['artistID'].values
+    weights = data_frame['weight'].values
+    
+    model = MF.MF(users, items, weights)
+    result = [] #List of calculated RMSE between model prediction and test set for each number of latent factors
+
+    for i in range(5,101):
+        model.train(n_latent_factors=i)
+        result.append([i, model.evaluate])
+
+    return result
+
+def dataset_2():
+    data_frame
 
 def main():
+    hetrec_result = dataset_1()
+    
     print("Done")
 if __name__ == "__main__":
     main()
--- matrix_factorization.py
+++ matrix_factorization.py
@@ -3,39 +3,38 @@ import pandas as pd
 from scipy import sparse
 from sklearn.metrics import root_mean_squared_error
 
-class matrix_factorization():
+class MF():
 
-    def __init__(self, users, items, weights, n_latent_factors):
+    def __init__(self, users, items, weights):
 
         #Put data into matrix
         self.matrix = sparse.coo_matrix((weights, (users, items))).tocsr()
 
         #Training and test sets (TODO add seed when dividing into sets)
         self.set = [(users[i], items[i]) for i in range(len(users))]
-        np.random.shuffle(set)
-        self.train = set[0:(int)(np.floor(0.75*len(set)))] # training set, 75% of data
-        self.test = set[(int)(np.floor(0.75*len(set))):len(set)+1] # test set, 25% of data
+        np.random.shuffle(self.set)
+        self.training = self.set[0:(int)(np.floor(0.75*len(self.set)))] # training set, 75% of data
+        self.test = self.set[(int)(np.floor(0.75*len(self.set))):len(self.set)+1] # test set, 25% of data
 
         #Parameters
-        self.n_latent_factors = n_latent_factors
         self.max_passes = 100
         self.step_size = 0.1
         self.reg = 0.01 #Regularization factor
 
-    def train(self):
-
+    def train(self, n_latent_factors):
+        self.n_latent_factors = n_latent_factors
         #Initialize user and item vectors to random values
         self.user_vectors = np.random.normal(size=(np.max(self.users), self.n_latent_factors))
         self.item_vectors = np.random.normal(size=(np.max(self.items), self.n_latent_factors))
 
         for i in range(self.max_passes):
             #Shuffle the training set
-            np.random.shuffle(self.train)
+            np.random.shuffle(self.training)
             self.SGD()
 
     def SGD(self):
-        for i in self.train:
-            u, m = self.train[i]
+        for i in self.training:
+            u, m = self.training[i]
             error = self.calc_error(u, m)
             temp = self.user_vectors
             self.user_vectors[u, :] -= self.step_size * (error * self.item_vectors - self.reg * self.user_vectors[u, :])
@@ -45,3 +44,15 @@ class matrix_factorization():
         prediction = np.dot(self.users(u), self.items(m))
         actual = self.matrix[u, m]
         return actual - prediction
+    
+    def get_rating(self, user, item):
+        return np.dot(self.user_vectors[user], self.item_vectors[item])
+
+    def evaluate(self):
+        predictions = []
+        actual = []
+        for i in self.test:
+            u, m = self.test[i]
+            predictions.append(self.get_rating(u, m))
+            actual.append(self.matrix[u, m])
+        return root_mean_squared_error(actual, predictions)
